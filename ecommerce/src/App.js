2d2b897eac2a3732bad153e81233f1d699084608
Pratyaksh7 <pratyakshgupta7@gmail.com>
2020-12-08 20:13:53 +0530

    few small changes

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="app">
         {/* <h1>This will be Ecommerce project</h1> */}
         <Header />
-        {/* Home */}
+        <Home />
     </div>
   );
 }
