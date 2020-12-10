2635db99a88c431b9165d4ef25755135016f8158
Pratyaksh7 <pratyakshgupta7@gmail.com>
2020-12-10 15:21:56 +0530

    added context api for checkout

--- ecommerce/src/Product.js
+++ ecommerce/src/Product.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 function Product({id, title, price, image, rating}) {
+
+    const [{basket}, dispatch] = useStateValue();
+
+    console.log(basket);
+    const addToBasket = () => {
+        // dispatch the item to the data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title:title,
+                price:price,
+                image:image,
+                rating:rating
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="product_info">
@@ -19,7 +38,7 @@ function Product({id, title, price, image, rating}) {
             </div>
             
             <img src={image} alt="" />
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
